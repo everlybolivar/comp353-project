58370424b80ecbe265a1a489ec61ea458f174704
igor <igor3volf@mail.ru>
2018-08-09 00:07:47 -0400

    Check for duplicated Email on Register page

--- RegisterValidation.php
+++ RegisterValidation.php
@@ -1,27 +1,27 @@
 <?php
-$host = 'ddc353.encs.concordia.ca';
-$username = 'ddc353_1';
-$password = '353DBpro';
-$db = 'ddc353_1';
-$db_port = '3306';
+require 'DB.php';
+
 
 function Register($fName, $lName, $email, $pw, $role)
 {
-    $host = 'ddc353.encs.concordia.ca';
-    $username = 'ddc353_1';
-    $password = '353DBpro';
-    $db = 'ddc353_1';
-    $db_port = '3306';
-    $connection = mysqli_connect($host, $username, $password, $db, $db_port);
+    $connection = DB::getConnection();
     if ($connection->connect_error) {
         die("error failure" . $connection->connect_error);
+    }  else if ($role == "client") {
+        $id = null;
+
+        $sql = $connection->prepare("INSERT INTO users (email,password) VALUES(?,?)");
+        $sql->bind_param("ss", $email,$pw);
+        $sql->execute();
+        $connection->close();
+
     } else {
-        $inital = $fName[0].$lName[0];
+        $initial = $fName[0].$lName[0];
 
         if($role == "employee"){
             $id = 7;
             $sql = $connection->prepare("INSERT INTO employee (employee_fname,employee_lname,employee_initial,department_id) VALUES(?,?,?,?)");
-            $sql->bind_param("ssss", $fName, $lName,$inital,$id);
+            $sql->bind_param("ssss", $fName, $lName,$initial,$id);
             $sql->execute();
 
         }
@@ -29,7 +29,7 @@ function Register($fName, $lName, $email, $pw, $role)
         if($role == "manager"){
             $id = 1;
             $sql = $connection->prepare("INSERT INTO employee (employee_fname,employee_lname,employee_initial,department_id) VALUES(?,?,?,?)");
-            $sql->bind_param("ssss", $fName, $lName,$inital,$id);
+            $sql->bind_param("ssss", $fName, $lName,$initial,$id);
             $sql->execute();
 
         }
@@ -37,7 +37,7 @@ function Register($fName, $lName, $email, $pw, $role)
         if($role == "admin"){
             $id = 13;
             $sql = $connection->prepare("INSERT INTO employee (employee_fname,employee_lname,employee_initial,department_id) VALUES(?,?,?,?)");
-            $sql->bind_param("ssss", $fName, $lName,$inital,$id);
+            $sql->bind_param("ssss", $fName, $lName,$initial,$id);
             $sql->execute();
 
         }
@@ -45,7 +45,7 @@ function Register($fName, $lName, $email, $pw, $role)
         if($role == "salesAssociate"){
             $id = 14;
             $sql = $connection->prepare("INSERT INTO employee (employee_fname,employee_lname,employee_initial,department_id) VALUES(?,?,?,?)");
-            $sql->bind_param("ssss", $fName, $lName,$inital,$id);
+            $sql->bind_param("ssss", $fName, $lName,$initial,$id);
             $sql->execute();
 
         }
@@ -55,8 +55,25 @@ function Register($fName, $lName, $email, $pw, $role)
         $sql = $connection->prepare("INSERT INTO users (email,password,employee_id) VALUES(?,?,?)");
         $sql->bind_param("sss", $email,$pw,$employee_id);
         $sql->execute();
-
         $connection->close();
+    }
+}
+
+function duplicateEmail($newEmail){
+    $conn = DB::getConnection();
+    if ($conn->connect_error) {
+        die("error failure" . $conn->connect_error);
+    }
+    else
+        {
+        $result = mysqli_query($conn,"SELECT * FROM users");
+        while($emailList=mysqli_fetch_array($result)){
+            if(in_array($newEmail,$emailList)){
+                return true;
+            }
+        }
+        return false;
+
     }
 }
 
