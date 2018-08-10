97add80cd03293d48384d4157a134fdc2ee35673
Ronnie Pang <ronn.pang@gmail.com>
2018-08-10 17:22:40 -0400

    Added user reports

---
+++ ManagerContract.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ronniepang
+ * Date: 2018-08-06
+ * Time: 9:40 PM
+ */
---
+++ ManagerEmployees.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ronniepang
+ * Date: 2018-08-07
+ * Time: 12:33 AM
+ */
